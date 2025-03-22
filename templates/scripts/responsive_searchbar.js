c2d684426576bfc44bd80074353fcb9be907d44d
lukasmichalcak <xmichalcak@stuba.sk>
2025-03-22 18:17:00 +0100

    responsive and uniform searchbar across all screens

--- templates/scripts/responsive_searchbar.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("search-toggle").addEventListener("click", function() {
-    let searchForm = document.querySelector(".search-form");
-    searchForm.style.display = (searchForm.style.display === "flex") ? "none" : "flex";
-});
-
