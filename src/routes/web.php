05313e9758b6fbb40a3ea004e71090c3077b87b4
KiZuH <Jozefjojo897@protonmail.com>
2025-04-09 18:45:57 +0200

    Adding login page

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,3 +5,7 @@ use App\Http\Controllers\HomeController;
 
 Route::resource('/', HomeController::class);
 
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product.show');
+
+Route::get('/login', [App\Http\Controllers\Auth\LoginController::class, 'showLoginForm'])->name('login');
+Route::post('/login', [App\Http\Controllers\Auth\LoginController::class, 'login']);
